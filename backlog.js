a7003eea1848ba78262501f4618c087f5fbe92a0
Cagri Avsar <c.avsar@hotmail.com>
2021-10-19 20:24:46 +0200

    changes in backlog.js

--- backlog.js
+++ backlog.js
@@ -8,10 +8,43 @@ async function init() {
 
 
 function addUser() {
-    users.push(username.value);
+    let title = document.getElementById('title-input').value;
+    let category = document.getElementById('category-dropdown').value;
+    let description = document.getElementById('description-input').value;
+    let duedate = document.getElementById('due-date-input').value;
+    let urgency = document.getElementById('urgency-dropdown').value;
+    let assignedto = document.getElementById('assigned-to').value;
+
+    users.push(task.value);
     backend.setItem('users', JSON.stringify(users));
+
+    let task = {
+        'title': title,
+        'category': category,
+        'description': description,
+        'duedate': duedate,
+        'urgency': urgency,
+        'assigned to': assignedto,
+        'createdate': new Date().getTime()
+    };
+
+    renderBackLogs()
 }
 
 function deleteUser(name) {
     backend.deleteItem('users');
+}
+
+function renderBackLogs() {
+    let backlogsContainer = document.getElementById('backlogsContainer');
+
+    backlogsContainer.innerHTML = ``;
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        backlogsContainer.innerHTML = `
+        <div class="task-info-container">
+
+        </div>
+        `;
+    }
 }
