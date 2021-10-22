527d03f3eb29d79e9e57f9291ff06abc9637768f
Cagri Avsar <c.avsar@hotmail.com>
2021-10-22 16:09:47 +0200

    new structure

--- js/backlog.js
+++ js/backlog.js
@@ -1,27 +1,50 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
+let users = [];
+
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+    setURL('http://developerakademie.com/smallest_backend_ever');
+}
+
+
+function addUser() {
+    let title = document.getElementById('title-input').value;
+    let category = document.getElementById('category-dropdown').value;
+    let description = document.getElementById('description-input').value;
+    let duedate = document.getElementById('due-date-input').value;
+    let urgency = document.getElementById('urgency-dropdown').value;
+    let assignedto = document.getElementById('assigned-to').value;
+
+    users.push(task.value);
+    backend.setItem('users', JSON.stringify(users));
+
+    let task = {
+        'title': title,
+        'category': category,
+        'description': description,
+        'duedate': duedate,
+        'urgency': urgency,
+        'assigned to': assignedto,
+        'createdate': new Date().getTime()
+    };
+
+    renderBackLogs()
+}
+
+function deleteUser(name) {
+    backend.deleteItem('users');
+}
+
+function renderBackLogs() {
+    let backlogsContainer = document.getElementById('backlogsContainer');
+
+    backlogsContainer.innerHTML = ``;
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        backlogsContainer.innerHTML = `
+        <div class="task-info-container">
+
+        </div>
+        `;
     }
-  }
+}
