6fda6f2081caf72585532bdf691daeafb24052f6
Marian Will <marianwillspam@gmail.com>
2021-10-23 15:32:45 +0200

    navbar in addtask

--- js/addTask.js
+++ js/addTask.js
@@ -1,36 +1,4 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-        /* Make an HTTP request using the attribute value as the file name: */
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-            /* Remove the attribute, and call this function once more: */
-            elmnt.removeAttribute("w3-include-html");
-            includeHTML();
-          }
-        }
-        xhttp.open("GET", file, true);
-        xhttp.send();
-        /* Exit the function: */
-        return;
-      }
-    }
-  }
-
   let allTasks = [];
-
-
- 
-
 
   function addTask(){
     let title = document.getElementById('title-input').value; 
@@ -48,6 +16,7 @@ function includeHTML() {
     console.log('The Due Date is ', duedate);
     console.log('The Urgency is ', urgency);
     console.log('The Assigned to is ', assignedto); /* funktioniert noch nicht */
+
 
     let task = {
       'title': title,
